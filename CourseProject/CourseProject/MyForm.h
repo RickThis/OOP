70bdc321f5dd036a4b36e753b3e618725c81a229
Ярослав Римарчук <Yaroslav.Rymarchuk@cs.khpi.edu.ua>
2021-12-02 19:17:01 +0200

    FinalFix

--- CourseProject/CourseProject/MyForm.h
+++ CourseProject/CourseProject/MyForm.h
@@ -1,6 +1,6 @@
 #pragma once
 
-namespace CourseProject {
+namespace Course {
 
 	using namespace System;
 	using namespace System::ComponentModel;
@@ -34,12 +34,30 @@ namespace CourseProject {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Label^ label1;
+	protected:
+
+	private: System::Windows::Forms::Label^ label2;
+
+	protected:
+
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::TextBox^ textBox3;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::Label^ label4;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Label^ label5;
+	private: System::Windows::Forms::Label^ label6;
+
+
+	protected:
 
 	private:
 		/// <summary>
 		/// Обязательная переменная конструктора.
 		/// </summary>
-		System::ComponentModel::Container ^components;
+		System::ComponentModel::Container^ components;
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
@@ -48,12 +66,148 @@ namespace CourseProject {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"MyForm";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->label6 = (gcnew System::Windows::Forms::Label());
+			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(51, 9);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(269, 17);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"Enter coefficients and then tap the button";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(164, 68);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(16, 17);
+			this->label2->TabIndex = 1;
+			this->label2->Text = L"a";
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(100, 88);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(154, 22);
+			this->textBox1->TabIndex = 2;
+			this->textBox1->TextChanged += gcnew System::EventHandler(this, &MyForm::textBox1_TextChanged);
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(100, 133);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(154, 22);
+			this->textBox2->TabIndex = 3;
+			// 
+			// textBox3
+			// 
+			this->textBox3->Location = System::Drawing::Point(100, 178);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(154, 22);
+			this->textBox3->TabIndex = 4;
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Location = System::Drawing::Point(164, 113);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(16, 17);
+			this->label3->TabIndex = 5;
+			this->label3->Text = L"b";
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Location = System::Drawing::Point(164, 158);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(15, 17);
+			this->label4->TabIndex = 6;
+			this->label4->Text = L"c";
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(100, 228);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(153, 25);
+			this->button1->TabIndex = 7;
+			this->button1->Text = L"Solution";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->Location = System::Drawing::Point(51, 285);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(0, 17);
+			this->label5->TabIndex = 8;
+			// 
+			// label6
+			// 
+			this->label6->AutoSize = true;
+			this->label6->Location = System::Drawing::Point(51, 315);
+			this->label6->Name = L"label6";
+			this->label6->Size = System::Drawing::Size(0, 17);
+			this->label6->TabIndex = 9;
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(350, 372);
+			this->Controls->Add(this->label6);
+			this->Controls->Add(this->label5);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->textBox3);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Name = L"MyForm";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
 		}
 #pragma endregion
+	private: System::Void textBox1_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+
+	}
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		this->label5->Text = "";
+		this->label6->Text = "";
+
+		double a = System::Convert::ToDouble(this->textBox1->Text);
+		double b = System::Convert::ToDouble(this->textBox2->Text);
+		double c = System::Convert::ToDouble(this->textBox3->Text);
+		double dis = (b * b) - 4 * a * c;
+		if (dis > 0) {
+			double res1 = -b + System::Math::Sqrt(dis);
+			double res2 = -b - System::Math::Sqrt(dis);
+			this->label5->Text = "Solution 1:" + System::Convert::ToString(res1 / (2 * a));
+			this->label6->Text = "Solution 2:" + System::Convert::ToString(res2 / (2 * a));
+		}
+		else if (dis == 0) {
+			double res1 = -b + System::Math::Sqrt(dis);
+			this->label5->Text = "Solution 1:" + System::Convert::ToString(res1 / (2 * a));
+		}
+		else {
+			this->label5->Text = "The equation does not have any solutions";
+
+		}
+	}
 	};
 }
