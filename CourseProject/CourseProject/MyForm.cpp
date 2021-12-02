30846c14980bb0ccb99c950065bbb7319698a52c
Ярослав Римарчук <Yaroslav.Rymarchuk@cs.khpi.edu.ua>
2021-12-02 19:14:07 +0200

    Fixed_2

--- CourseProject/CourseProject/MyForm.cpp
+++ CourseProject/CourseProject/MyForm.cpp
@@ -1,2 +1,10 @@
-#include "MyForm.h"
 
+#include "MyForm.h"
+#include <Windows.h>
+using namespace CourseProject; // Название проекта
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	Application::Run(gcnew MyForm);
+	return 0;
+}
