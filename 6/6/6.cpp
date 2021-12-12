3d79400936ee31e3dc0017d7fdc608adb102a32b
Ярослав Римарчук <Yaroslav.Rymarchuk@cs.khpi.edu.ua>
2021-12-12 23:12:15 +0200

    Done

--- 6/6/6.cpp
+++ 6/6/6.cpp
@@ -2,19 +2,35 @@
 //
 
 #include <iostream>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <conio.h>
+#include <iomanip>
+
 
 using namespace std;
 
 class Airport {
-	string place;
-	int throughput;
-	int capacity;
 public:
-	Airport();
-	void change(string&, int&, int&);
-
+    string place;
+    int throughput;
+    int capacity;
 	friend ostream& operator <<(ostream& os, Airport& other);
 };
+
+void change(Airport& a, string p, int t, int c) {
+    a.capacity = c;
+    a.place = p;
+    a.throughput;
+}
+void clrscr() {
+    system("CLS");
+}
+
+
+Airport obj[7];
+
 
 ostream& operator << (ostream& os, Airport& other) {
 	os << other.place << " " << other.throughput <<
@@ -22,25 +38,102 @@ ostream& operator << (ostream& os, Airport& other) {
 	return os;
 }
 
-void Airport::change(string& p, int& t, int& c) {
-	this->place = p;
-	this->throughput = t;
-	this->capacity = c;
-}
-
-Airport::Airport() {
-	throughput = 0;
-	capacity = 0;
-}
-
-
-
-int main()
+void sort()
 {
-	Airport* a[7];
-	
-	
+    int i, j;
+    Airport temp;
+    for (i = 0; i < 7; i++)
+        for (j = i + 1; j < 7; j++)
+            if (obj[i].capacity > obj[j].capacity) {
+                temp = obj[i];
+                obj[i] = obj[j];
+                obj[j] = temp;
+            }
+}
 
+void show()
+{
+    int i, j;
+    cout << "                            ТЕЛЕФОН: \n";
+    cout << "----------------------------------------------------------------\n";
+    cout << "        Capacity        Throughput               Place     \n";
+    cout << "----------------------------------------------------------------\n";
+
+    char s[10];
+    for (i = 0; i < 7; i++) {
+        if (strlen(s) == 6) {
+            cout << ' ' << obj[i].capacity;
+        }
+        else if (strlen(s) == 7) {
+            cout << s[0] << s[1] << s[2] << "-" << s[3] << s[4] << "-" << s[5] << s[6];
+        }
+        cout << setw(15) << obj[i].capacity;
+        if (obj[i].throughput == 0) cout << setw(15) << "Нет";
+        else if (obj[i].throughput == 1) cout << setw(15) << "Да";
+        cout << setw(25) << obj[i].place;
+        cout << "\n";
+    }
+}
+
+
+int main(void) {
+
+    setlocale(LC_ALL, "rus");
+    int r = 0;
+
+    for (int a = 0; !a;) {
+        clrscr();
+        cout << "1. Добавить записи\n";
+        cout << "2. Сортировать базу\n";
+        cout << "3. Вывести базу\n";
+        cout << "4. Выход\n";
+        cout << "> ";
+        int p;
+        cin >> p;
+        switch (p) {
+        case 1: {
+            change(obj[0],"Sharimetevo", 123, 123);
+            change(obj[1], "Sharimetevo", 456779, 456779);
+            change(obj[2], "Sharimetevo", 12443, 12443);
+            change(obj[3], "Sharimetevo", 13, 13);
+            change(obj[4], "Sharimetevo", 1267853, 1267853);
+            change(obj[5], "Sharimetevo", 12113, 12113);
+            r = 1;
+            break;
+        }
+
+        case 2: {
+            if (r == 0) {
+                cout << "В базе нет записей.";
+                _getch();
+            }
+            else if (r == 1) sort();
+            break;
+        }
+
+        case 3: {
+            if (r == 0) {
+                cout << "В базе нет записей.";
+                _getch();
+            }
+            else if (r == 1)
+                show();
+            _getch();
+            break;
+        }
+
+        case 4: {
+            a = 1;
+            break;
+        }
+
+        default: {
+            cout << "Неверный вызов";
+            _getch();
+            break;
+        }
+        }
+    }
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
